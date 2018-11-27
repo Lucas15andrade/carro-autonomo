aa6b739c70dddc07a607c083c411e1c56c9576cf
Lucas Andrade <lap.andrade15@gmail.com>
2018-11-27 13:23:53 -0300

    Revert "teste radar"
    This reverts commit 6c7114663de97ba4386306c7159d77b7dbd0d4d3.

--- app/src/main/java/br/ufrn/eaj/tads/carroautnomo/MainActivity.java
+++ app/src/main/java/br/ufrn/eaj/tads/carroautnomo/MainActivity.java
@@ -7,65 +7,31 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import processing.core.PApplet;
-
 public class MainActivity extends AppCompatActivity {
 
     private Button estacionar;
     private Button mapear;
     private Button manual;
 
-    private PApplet sketch;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sketch = new Sketch();
-
     }
 
-    public void abaEstacionar(View v) {
+    public void abaEstacionar(View v){
         Intent i = new Intent(MainActivity.this, EstacionamentoActivity.class);
         startActivity(i);
     }
 
-    public void abaMapeamento(View v) {
+    public void abaMapeamento(View v){
         Intent i = new Intent(MainActivity.this, MapeamentoActivity.class);
         startActivity(i);
     }
 
-    public void abaManual(View v) {
+    public void abaManual(View v){
         Intent i = new Intent(MainActivity.this, ManualActivity.class);
         startActivity(i);
     }
-
-////    @Override
-////    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-////        if (sketch != null) {
-////            sketch.onRequestPermissionsResult(requestCode, permissions, grantResults);
-////        }
-////    }
-////
-////    @Override
-////    public void onNewIntent(Intent intent) {
-////        if (sketch != null) {
-////            sketch.onNewIntent(intent);
-////        }
-////    }
-////
-////    @Override
-////    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-////        if (sketch != null) {
-////            sketch.onActivityResult(requestCode, resultCode, data);
-////        }
-////    }
-////
-////    @Override
-////    public void onBackPressed() {
-////        if (sketch != null) {
-////            sketch.onBackPressed();
-////        }
-//    }
 }
